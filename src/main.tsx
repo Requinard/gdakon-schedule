16cc34a24743a3ef8f8f420b37f68e8783f226ec
requinard <david@requinard.nl>
2022-12-15 21:44:02 +0100

    add mui

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,22 @@
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+
+const theme = createTheme({
+    palette: {
+        mode: "dark",
+    },
+});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
-        <p>Hello world</p>
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+        </ThemeProvider>
     </React.StrictMode>
 );
