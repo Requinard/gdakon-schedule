62d101926d97321f927693603a2d15fce801e1ae
requinard <david@requinard.nl>
2022-12-27 22:03:33 +0100

    add bookmark indicator to the home page

--- src/components/EventSchedule/EventListItem.tsx
+++ src/components/EventSchedule/EventListItem.tsx
@@ -1,8 +1,16 @@
-import { ListItem, ListItemText } from "@mui/material";
+import {
+    IconButton,
+    ListItem,
+    ListItemSecondaryAction,
+    ListItemText,
+} from "@mui/material";
 
 import { NormalizedEventScheduleItem } from "../../store/gdakon.types";
 import { useLocalizedEvent } from "../../hooks/useLocalizedEvent";
 import { useEventSubtitle } from "../../hooks/useEventSubtitle";
+import { useAppSelector } from "../../store";
+
+import BookmarkIcon from "~icons/mdi/bookmark";
 
 type EventListItemProps = {
     event: NormalizedEventScheduleItem;
@@ -11,11 +19,22 @@ type EventListItemProps = {
 export const EventListItem = ({ event }: EventListItemProps) => {
     const { name, room } = useLocalizedEvent(event);
 
+    const isBookmarked = useAppSelector((state) =>
+        state.bookmarks.events.includes(event.id)
+    );
+
     const subtitle = useEventSubtitle(event, room);
 
     return (
         <ListItem>
             <ListItemText primary={name} secondary={subtitle} />
+            {isBookmarked && (
+                <ListItemSecondaryAction>
+                    <IconButton color={"warning"}>
+                        <BookmarkIcon />
+                    </IconButton>
+                </ListItemSecondaryAction>
+            )}
         </ListItem>
     );
 };
