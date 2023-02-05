2610cf706a78fc14529330c1b4d371e6331fd9d7
requinard <david@requinard.nl>
2023-02-05 17:41:36 +0100

    fix minor issue with service worker

--- src/components/Common/ServiceWorker.tsx
+++ src/components/Common/ServiceWorker.tsx
@@ -3,8 +3,11 @@ import { useEffect, useState } from "react";
 import { registerSW } from "virtual:pwa-register";
 import { Alert, Button } from "@mui/material";
 import { noop } from "lodash";
+
 export const ServiceWorker = () => {
-    const [update, setUpdate] = useState<() => void>(noop);
+    const [update, setUpdate] = useState<
+        ((reload: boolean) => Promise<void>) | null
+    >(null);
     const [needRefresh, setNeedRefresh] = useState(false);
 
     useEffect(() => {
@@ -13,16 +16,16 @@ export const ServiceWorker = () => {
                 onNeedRefresh: () => setNeedRefresh(true),
             })
         );
-    });
+    }, [setUpdate, setNeedRefresh]);
 
-    if (!needRefresh && update !== undefined) {
+    if (!needRefresh || update === null) {
         return null;
     }
 
     return (
         <Alert
             severity={"warning"}
-            action={<Button onClick={update}>Update</Button>}
+            action={<Button onClick={() => update(true)}>Update</Button>}
         >
             A new version is available
         </Alert>
