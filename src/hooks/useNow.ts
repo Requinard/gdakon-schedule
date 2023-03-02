850e5a78b80ef37f0b3653042aaba44a29cbbaad
requinard <david@requinard.nl>
2023-03-02 11:08:39 +0100

    make useNow return a new time object every 30 seconds

--- src/hooks/useNow.ts
+++ src/hooks/useNow.ts
@@ -1,20 +1,36 @@
-import { useMemo } from "react";
+import { useEffect, useState } from "react";
 import dayjs, { Dayjs } from "dayjs";
+import { useInterval } from "usehooks-ts";
 
 import { useAppSelector } from "../store";
 import { useLocale } from "../i18n";
 
-export const useNow = () => {
+const calculateNow = (
+    enabled: boolean,
+    amount: number,
+    locale: string
+): Dayjs =>
+    enabled
+        ? dayjs().locale(locale).add(amount, "milliseconds")
+        : dayjs().locale(locale);
+
+export const useNow = (): Dayjs => {
     const { locale } = useLocale();
     const { enabled, amount } = useAppSelector(
         (state) => state.settings.timetravel
     );
 
-    return useMemo((): Dayjs => {
-        if (enabled) {
-            return dayjs().locale(locale).add(amount, "milliseconds");
-        }
+    const [now, setNow] = useState(calculateNow(enabled, amount, locale));
 
-        return dayjs();
-    }, [enabled, amount, locale]);
+    useEffect(() => {
+        setNow(calculateNow(enabled, amount, locale));
+    }, [setNow, enabled, amount, locale]);
+
+    useInterval(() => {
+        const newNow = calculateNow(enabled, amount, locale);
+
+        setNow(newNow);
+    }, 30_000);
+
+    return now;
 };
