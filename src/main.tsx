37cf7ad7933fbd5eb2944ffbbeaefba4f336a0a0
requinard <david@requinard.nl>
2024-02-26 22:51:40 +0100

    persist stuff to cache

--- src/main.tsx
+++ src/main.tsx
@@ -1,30 +1,22 @@
 import "@fontsource/roboto/latin.css";
-
-import "./i18n";
 import "./utilities/dayjs";
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { CssBaseline } from "@mui/material";
 import { BrowserRouter } from "react-router-dom";
 import { Provider as StoreProvider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+import { LocaleProvider } from "./i18n";
 import { App } from "./App";
 import { persistor, store } from "./store";
-import { LocaleProvider } from "./i18n/LocalizationProvider";
 
-import { AppThemeProvider } from "~providers";
-
-const queryClient = new QueryClient({
-    defaultOptions: {
-        queries: { staleTime: 5 * 60 * 1000 /* 5 minutes */ },
-    },
-});
+import { AppQueryClientProvider, AppThemeProvider } from "~providers";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
-        <QueryClientProvider client={queryClient}>
+        <AppQueryClientProvider>
             <StoreProvider store={store}>
                 <AppThemeProvider>
                     <LocaleProvider>
@@ -37,6 +29,6 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
                     </LocaleProvider>
                 </AppThemeProvider>
             </StoreProvider>
-        </QueryClientProvider>
+        </AppQueryClientProvider>
     </React.StrictMode>
 );
