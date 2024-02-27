929cee9736aa29dd8df8361b850e935161b4de61
Patroll <6214736+P4tr0ll@users.noreply.github.com>
2024-02-27 16:56:47 +0100

    feat: add no results text in event search

--- src/components/EventSchedule/EventListVirtualized.tsx
+++ src/components/EventSchedule/EventListVirtualized.tsx
@@ -20,9 +20,7 @@ type VirtuosoProps<T> = {
 };
 
 export const EventListVirtualized = () => {
-    const { t } = useTranslation("EventSchedule", {
-        keyPrefix: "Groups",
-    });
+    const { t } = useTranslation("EventSchedule");
     const now = useNow();
     const { filtered = [] } = useEventFilter();
 
@@ -36,7 +34,7 @@ export const EventListVirtualized = () => {
             );
 
             if (expired.length) {
-                groups[t("expired")] = expired;
+                groups[t("Groups.expired")] = expired;
             }
 
             const groupCounts = map(groups, (values) => values.length);
@@ -55,36 +53,57 @@ export const EventListVirtualized = () => {
                     <EventSearch />
                 </Box>
             </Container>
-            <GroupedVirtuoso<EventScheduleItemModel>
-                groupCounts={groupCounts}
-                height={"100%"}
-                groupContent={(index) => (
-                    <Container key={groups[index]}>
-                        <Box pt={2} pb={2} pl={2} bgcolor={"background.paper"}>
-                            <Typography variant={"h5"}>
-                                {groups[index]}
-                            </Typography>
-                        </Box>
-                    </Container>
-                )}
-                itemContent={(index, groupIndex) => {
-                    const item = items[index];
-                    return (
-                        <Container
-                            sx={{
-                                pb: 1,
-                                opacity:
-                                    groups[groupIndex] === t("expired")
-                                        ? 0.4
-                                        : 1,
-                            }}
-                            key={item.id}
-                        >
-                            <EventScheduleItemCard event={item} />
+            {items.length === 0 ? (
+                <Box
+                    display={"flex"}
+                    flexDirection={"column"}
+                    flex={1}
+                    alignItems={"center"}
+                    justifyContent={"center"}
+                    gap={2}
+                >
+                    <Typography variant={"h5"}>
+                        {t("Search.no_results")}
+                    </Typography>
+                </Box>
+            ) : (
+                <GroupedVirtuoso<EventScheduleItemModel>
+                    groupCounts={groupCounts}
+                    height={"100%"}
+                    groupContent={(index) => (
+                        <Container key={groups[index]}>
+                            <Box
+                                pt={2}
+                                pb={2}
+                                pl={2}
+                                bgcolor={"background.paper"}
+                            >
+                                <Typography variant={"h5"}>
+                                    {groups[index]}
+                                </Typography>
+                            </Box>
                         </Container>
-                    );
-                }}
-            />
+                    )}
+                    itemContent={(index, groupIndex) => {
+                        const item = items[index];
+                        return (
+                            <Container
+                                sx={{
+                                    pb: 1,
+                                    opacity:
+                                        groups[groupIndex] ===
+                                        t("Groups.expired")
+                                            ? 0.4
+                                            : 1,
+                                }}
+                                key={item.id}
+                            >
+                                <EventScheduleItemCard event={item} />
+                            </Container>
+                        );
+                    }}
+                />
+            )}
         </Box>
     );
 };
